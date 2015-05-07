59a15730eb01808e88e111a5eceff7ff3dab4793
Mark Roland <radiowhir@gmail.com>
2015-05-06 21:23:49 -0700

    Initial Commit

---
+++ src/ArcGIS.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace markroland\ArcGIS;
+
+/**
+ *
+ * A PHP class for interacting with ArcGIS
+ *
+ * @author Mark Roland
+ * @copyright 2015 Mark Roland
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/markroland/arcgis-php
+ *
+ **/
+class ArcGIS
+{
+
+}
